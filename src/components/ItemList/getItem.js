85dec35ac983c40566ebf5315b0a4a9e0d24ad16
espacioobligado <143134998+espacioobligado@users.noreply.github.com>
2023-09-07 16:56:39 -0300

    Updates

--- src/components/ItemList/getItem.js
+++
@@ -1,9 +0,0 @@
-import productos from "./data"
-
-export const getItem = () => {
-    return new Promise((resolve,reject) => {
-        setTimeout( () => {
-            resolve(productos)
-        } , 1000)
-    })
-}
